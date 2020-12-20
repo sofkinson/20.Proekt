24f3b51757998544f85313c4113b60f77ee10382
sofkinson <71452158+sofkinson@users.noreply.github.com>
2020-12-20 21:38:25 +0200

    Add files via upload

---
+++ ComicBook.java
@@ -0,0 +1,29 @@
+package myonlineshop;
+
+public class ComicBook extends Book {
+	
+private String color;; 
+	
+	public ComicBook(double regularPrice,
+            String publisher,
+            int yearPublished,
+            String color) {
+        super(regularPrice, publisher, yearPublished);
+        setColor(color);
+    }
+	
+	public double computeSalePrice(){
+        return super.getRegularPrice() * 0.4;
+    }
+
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+}
